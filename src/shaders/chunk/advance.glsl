faafb1f2521a7f1e8dd12ce302e93482d730339d
todaylg <lg2lugang@gmail.com>
2020-04-24 15:53:37 +0800

    Update:Default enable compensation

--- src/shaders/chunk/advance.glsl
+++ src/shaders/chunk/advance.glsl
@@ -15,16 +15,4 @@ float getEnergyCompensation(vec3 dfg, float f0) {
    return 1.0 + f0 * (1.0 / dfg.y - 1.0);
 }
 
-// Todo: Normal AA Test
-float adjustRoughness(float roughness, vec3 normal) {
-    // Based on The Order : 1886 SIGGRAPH course notes implementation (page 21 notes)
-    float normalLen = length(normal * 2.0 - 1.0);
-    if ( normalLen < 1.0) {
-        float normalLen2 = normalLen * normalLen;
-        float kappa = ( 3.0 * normalLen -  normalLen2 * normalLen )/( 1.0 - normalLen2 );
-        // http://www.frostbite.com/2014/11/moving-frostbite-to-pbr/
-        // page 91 : they use 0.5/kappa instead
-        return min(1.0, sqrt(roughness * roughness + 1.0 / kappa));
-    }
-    return roughness;
-}
+// Todo: NormalAA 、 MultiBounceAO
